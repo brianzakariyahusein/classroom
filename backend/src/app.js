ab03ff37f43154c1b5fb9a2e3ba688dc43ca141a
Brian Zakariya <zakariyabrian@gmail.com>
2025-07-20 00:42:39 +0700

    Create Server and App.js

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("./config/db");
+const authRoutes = require("./routes/auth");
+const classroomRoutes = require("./routes/classroom");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Route Test
+app.get("/api/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.use("/api/auth", authRoutes);
+// app.use("/api/classroom", classroomRoutes);
+
+module.exports = { app };
