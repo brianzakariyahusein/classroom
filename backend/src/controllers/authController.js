7ea9b9cd0db5d2c9c618854a1f594dda3c064e8c
Brian Zakariya <zakariyabrian@gmail.com>
2025-07-20 12:28:42 +0700

    make the controllers section neater and easier to maintenance,and also create the actual route function

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,66 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+// Register
+exports.register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  try {
+    const existing = await User.findOne({ email });
+    if (existing)
+      return res.status(400).json({ message: "Email already exists" });
+
+    const user = await User.create({ name, email, password, role });
+
+    res.status(201).json({
+      message: "User registered",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      token: generateToken(user),
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Login
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ message: "invalid! email or password is incorrect" });
+
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch)
+      return res
+        .status(400)
+        .json({ message: "invalid! email or password is incorrect" });
+
+    res.json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      token: generateToken(user),
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
