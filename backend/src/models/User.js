c9a681a8ea7e09a414dc049fcbc74ad1c56eadd5
Brian Zakariya <zakariyabrian@gmail.com>
2025-07-20 12:29:23 +0700

    Update User models

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,48 +1,45 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Please enter your name"],
-    },
-    email: {
-      type: String,
-      required: [true, "Please enter your email"],
-      unique: true,
-      lowercase: true,
-      trim: true,
-      validate: {
-        validator: function (value) {
-          // email validation regex
-          return /^[^\s@]+@(gmail\.com|yahoo\.com|outlook\.com)$/.test(value);
-        },
-        message: (props) =>
-          `${props.value} is not a valid email address! please use gmail.com, yahoo.com, or outlook.com`,
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator: function (value) {
+        // email validation regex
+        return /^[^\s@]+@(gmail\.com|yahoo\.com|outlook\.com)$/.test(value);
       },
-    },
-    password: {
-      type: String,
-      required: [true, "Please enter your password"],
-      minlength: [6, "Password must be at least 6 characters"],
-      validate: {
-        validator: function (value) {
-          // password validation regex
-          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/.test(value);
-        },
-        message:
-          "Password must contain at least one uppercase letter, one lowercase letter, and one number.",
-      },
-    },
-    role: {
-      type: String,
-      enum: ["teacher", "student"],
-      default: "student",
+      message: (props) =>
+        `${props.value} is not a valid email address! please use gmail.com, yahoo.com, or outlook.com`,
     },
   },
-  { timestamps: true }
-);
+  role: {
+    type: String,
+    enum: ["teacher", "student"],
+    default: "student",
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: [6, "Password must be at least 6 characters"],
+    validate: {
+      validator: function (value) {
+        // password validation regex
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/.test(value);
+      },
+      message:
+        "Password must contain at least one uppercase letter, one lowercase letter, and one number.",
+    },
+  },
+});
 
 // Enkripsi password sebelum simpan
 userSchema.pre("save", async function (next) {
